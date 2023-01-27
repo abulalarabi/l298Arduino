5b1a2f35b5a71ede087232791e0ab373d37ad9e5
abulalarabi <arabiabulal@gmail.com>
2023-01-27 14:26:31 -0600

    updated

--- L298.cpp
+++ L298.cpp
@@ -1,2 +1,76 @@
 //#include "Arduino.h"
 #include "L298.h"
+
+
+// drive function
+void L298::drive((int leftMotorSpeed, int rightMotorSpeed)
+{
+
+	// trim values between -255 and +255 for safety (8 bit)
+	leftMotorSpeed = leftMotorSpeed > 255 ? 255 : leftMotorSpeed;
+	leftMotorSpeed = leftMotorSpeed < -255 ? -255 : leftMotorSpeed;
+
+	rightMotorSpeed = rightMotorSpeed > 255 ? 255 : rightMotorSpeed;
+	rightMotorSpeed = rightMotorSpeed < -255 ? -255 : rightMotorSpeed;
+
+	// left motor control
+	if(leftMotorSpeed==0)			// left motor off
+	{
+		digitalWrite(IN1, 0);
+		analogWrite(IN2, 0);
+	}
+
+	else if(leftMotorSpeed>0)		// left motor forward
+	{
+		digitalWrite(IN1, 0);
+		analogWrite(IN2, leftMotorSpeed);
+		leftDir = false;
+	}
+
+	else							// left motor reverse
+	{
+		digitalWrite(IN1, 1);
+		analogWrite(IN2, 255 + leftMotorSpeed);
+		leftDir = true;
+	}
+
+
+	// right motor control
+	if(rightMotorSpeed==0)		// right motor off
+	{
+		digitalWrite(IN3, 0);
+		analogWrite(IN4, 0);
+	}
+
+	else if(rightMotorSpeed>0)	// right motor forward
+	{
+		digitalWrite(IN3, 0);
+		analogWrite(IN4, rightMotorSpeed);
+		rightDir = false;
+	}
+
+	else						// right motor reverse
+	{
+		digitalWrite(IN3, 0);
+		analogWrite(IN4, 255 + rightMotorSpeed);
+		rightDir = true;
+	}
+  
+}
+
+// brake function
+void L298::brake(unsigned int duration=20)
+{
+	// braking left motor
+	digitalWrite(IN1, !leftDir);
+	analogWrite(IN2, 128);
+
+	// braking right motor
+	digitalWrite(IN3, !leftDir);
+	analogWrite(IN4, 128);
+
+	delay(duration);
+
+	// release motors
+	drive(0,0);
+}
--- L298.h
+++ L298.h
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2023 Abul Al Arabi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include "Arduino.h"
 #ifndef L298_H
 #define L298_H
@@ -7,9 +31,36 @@ class L298{
     private:
         //bool SIXPIN = false;
         byte IN1, IN2, IN3, IN4;
+        bool leftDir = false;
+        bool rightDir = false;
+        bool esp;
 
     public:
-        //void init6(byte enA, byte in1, byte in2, byte in3, byte in4, byte enB);
-        void init(byte in1, byte in2, byte in3, byte in4);
+        L298(byte in1, byte in2, byte in3, byte in4, bool esp = false)
+        {
+            IN1 = in1;
+            IN2 = in2;
+            IN3 = in3;
+            IN4 = in4;
+            
+            pinMode(IN1, OUTPUT);
+            digitalWrite(IN1, LOW);
+            pinMode(IN3, OUTPUT);
+            digitalWrite(IN3, Low);
+            
+            /*
+            if(esp){
 
+                ledcAttachPin(IN2, 0);
+                ledcSetup(0, 1000, 8);
+
+                ledcAttachPin(IN4, 1);
+                ledcSetup(1, 1000, 8);
+            }
+            */
+        }
+        //void init6(byte enA, byte in1, byte in2, byte in3, byte in4, byte enB);
+        //void init(byte in1, byte in2, byte in3, byte in4, bool esp = false);
+        void drive(int leftMotorSpeed, int rightMotorSpeed);
+        void brake(unsigned int duration=20);
 }
